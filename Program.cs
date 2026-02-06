0d34d746c2aafcd3d6b78b02b18c3b3b978f93ae
Hyeon O <gusdh2510@gmail.com>
2026-02-06 16:06:29 +0900

    init : 실행 단위 구성

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Solutions;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        // 실행할 문제만 여기서 교체
+        new 문자열_겹쳐쓰기().Solve();
+        //new BOJ_1000().Solve();
+    }
+}
+
